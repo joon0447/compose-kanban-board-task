2fdd37ca475654123895032b1e4e20ea89f1d0fd
김조현 <118670117+noeyhoj@users.noreply.github.com>
2026-03-07 02:01:18 +0900

    [1단계 - 칸반 보드 태스크(카드)] 하로 미션 제출합니다. (#3)
    * fix: 기존의 App.kt를 체크 버튼으로 수정함
    
    * test: App.kt의 버튼 클릭에 대한 단위 테스트를 생성함
    
    * fix: 3개의 테스트코드를 동작하도록 수정함
    
    * docs: 이미지 파일을 추가함
    
    * chore: Icons 의존성을 추가함
    
    * feat: 0단계의 실습 코드를 작성함
    
    * feat: 1단계 칸반 보드의 각 케이스에 대한 함수를 작성함
    
    * docs: README.md 파일을 작성함
    
    * docs: 체크박스 및 디렉토리 변경
    
    * docs: 기존 README.md파일을 삭제함
    
    * docs: README.md에 페어를 추가함
    
    * refactor: size()의 값을 수정함
    
    * feat: 보드의 데이터를 관리하기 위해 클래스를 만듦
    
    * feat: 보드의 사용된 색상과 보드에 들어갈 내용과 같은 중복되는 내용을 상수로 관리하기 위해 파일을 만듦
    
    * refactor: step0의 예제 코드를 옮기고 보드 랜더링 로직을 리펙토링함
    
    * refactor: step0 코드를 따로 파일로 묶음
    
    * docs: README.md에 피드백 반영 내용을 추가함
    
    * fix: 에러가 나타나는 부분을 수정함
    
    * fix: 오류를 수정함
    
    * refactor: Step0파일을 study 패키지로 옮김
    
    * refactor: 자잘하게 수정됨
    
    * refactor: PreviewParameter를 사용하여 기존 BoardScreenview를 리펙토링함
    
    * test: Ui의 각 테스트 코드를 작성함
    
    * docs: README.md에 리펙토링 사항을 추가함

--- composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
+++ composeApp/src/commonMain/kotlin/woowacourse/kanban/board/App.kt
@@ -1,44 +1,180 @@
 package woowacourse.kanban.board
 
-import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.safeContentPadding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.foundation.layout.FlowRow
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
-import kanbanboard.composeapp.generated.resources.Res
-import kanbanboard.composeapp.generated.resources.compose_multiplatform
-import org.jetbrains.compose.resources.painterResource
+import androidx.compose.ui.tooling.preview.PreviewParameter
+import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import woowacourse.kanban.board.model.BORDER_COLOR
+import woowacourse.kanban.board.model.BoardComponent
+import woowacourse.kanban.board.model.CONTENT_COLOR
+import woowacourse.kanban.board.model.DEFAULT_CONTENT
+import woowacourse.kanban.board.model.DEFAULT_NAME
+import woowacourse.kanban.board.model.DEFAULT_TITLE
+import woowacourse.kanban.board.model.MAX_CONTENT
+import woowacourse.kanban.board.model.MAX_NAME
+import woowacourse.kanban.board.model.MAX_TITLE
+import woowacourse.kanban.board.model.PROFILE_BG_COLOR
+import woowacourse.kanban.board.model.PROFILE_COLOR
+import woowacourse.kanban.board.model.TAG_COLOR
 
 @Composable
-@Preview
 fun App() {
-    MaterialTheme {
-        var showContent by remember { mutableStateOf(false) }
-        Column(
-            modifier = Modifier
-                .background(MaterialTheme.colorScheme.primaryContainer)
-                .safeContentPadding()
-                .fillMaxSize(),
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Button(onClick = { showContent = !showContent }) {
-                Text("Click me!")
+}
+
+@Composable
+@Preview(showBackground = true)
+fun KanbanBoardTemplate(title: String = DEFAULT_TITLE, content: String = "", tags: List<String> = listOf(), name: String = DEFAULT_NAME) {
+    Box(
+        modifier = Modifier
+            .border(
+                width = 1.dp,
+                color = Color(BORDER_COLOR),
+                shape = RoundedCornerShape(15.dp),
+            )
+            .width(270.dp)
+            .padding(12.dp),
+    ) {
+        Column {
+            // 제목
+            Box(
+                modifier = Modifier.padding(vertical = 8.dp),
+            ) {
+                Text(
+                    title,
+                    fontSize = 16.sp,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                )
             }
-            AnimatedVisibility(showContent) {
-                Image(painterResource(Res.drawable.compose_multiplatform), null)
+            // 중간 내용
+            if (content.isNotBlank()) {
+                Box(
+                    modifier = Modifier.padding(vertical = 4.dp),
+                ) {
+                    Text(
+                        content,
+                        overflow = TextOverflow.Ellipsis,
+                        maxLines = 2,
+                        fontSize = 14.sp,
+                        color = Color(CONTENT_COLOR),
+                    )
+                }
+            }
+
+            // 태그
+            if (tags.isNotEmpty()) {
+                FlowRow(
+                    modifier = Modifier.padding(vertical = 8.dp),
+                    horizontalArrangement = Arrangement.spacedBy(8.dp),
+                    verticalArrangement = Arrangement.spacedBy(4.dp),
+                ) {
+                    for (tagIndex in tags.indices) {
+                        if (tagIndex < 5) {
+                            Box(
+                                modifier = Modifier
+                                    .background(
+                                        color = Color(TAG_COLOR),
+                                        shape = RoundedCornerShape(45.dp),
+                                    ),
+                            ) {
+                                Text(if (tags[tagIndex].length > 5) tags[tagIndex].substring(0, 5) else tags[tagIndex], modifier = Modifier.padding(6.dp), fontSize = 10.sp)
+                            }
+                        }
+                    }
+                }
+            }
+
+            // 구분선
+            HorizontalDivider(thickness = 2.dp)
+            // 작성자
+            Row(
+                modifier = Modifier.padding(vertical = 8.dp),
+                horizontalArrangement = Arrangement.spacedBy(6.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Person,
+                    contentDescription = null,
+                    tint = Color(PROFILE_COLOR),
+                    modifier = Modifier.size(25.dp)
+                        .clip(CircleShape)
+                        .border(width = 2.dp, color = Color(PROFILE_BG_COLOR))
+                        .background(color = Color(PROFILE_BG_COLOR)),
+                )
+                Text(
+                    name,
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1,
+                )
             }
         }
     }
+}
+
+class BoardPreviewParameterProvider : PreviewParameterProvider<BoardComponent> {
+    override val values = sequenceOf(
+        BoardComponent(
+            title = DEFAULT_TITLE,
+            content = DEFAULT_CONTENT,
+            tags = listOf("컴포넌트", "성능"),
+            name = DEFAULT_NAME,
+        ),
+        BoardComponent(
+            title = DEFAULT_TITLE,
+            tags = listOf("컴포넌트", "성능"),
+            name = DEFAULT_NAME,
+        ),
+        BoardComponent(
+            title = DEFAULT_TITLE,
+            content = DEFAULT_CONTENT,
+            name = DEFAULT_NAME,
+        ),
+        BoardComponent(
+            title = DEFAULT_TITLE,
+            name = DEFAULT_NAME,
+        ),
+        BoardComponent(
+            title = MAX_TITLE,
+            content = MAX_CONTENT,
+            tags = listOf("너무너무", "긴 태그", "최대로", "5자까지", "5개제한임"),
+            name = MAX_NAME,
+        ),
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun BoardScreenView(@PreviewParameter(BoardPreviewParameterProvider::class) board: BoardComponent) {
+    KanbanBoardTemplate(
+        title = board.title,
+        content = board.content,
+        tags = board.tags,
+        name = board.name,
+    )
 }
